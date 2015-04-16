ca2e6fed09de13d69ad8ce76e96ce057474032f1
Casey Newhouse Davis <casey davis@Caseys-MacBook-Air-4.local>
2015-04-15 17:08:55 -0700

    final styled Amazeriffic

---
+++ javascripts/functions.js
@@ -0,0 +1,11 @@
+//project 2 js functions
+
+var isArmstong = function(n){
+	"use strict";
+	//convert to string
+	n = String(n);
+	for (var i = 0; i< n.length; ++i){
+		var sum = sum + Math.pow(n[i], n.length)
+		return sum
+	}
+}
